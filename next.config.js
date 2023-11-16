ca8251a2bad0b4637a37f0322c89a50a89396910
Meilian Li <113812316+MeilianLee@users.noreply.github.com>
2023-11-16 13:16:03 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  title: `Project Website`,
+  tagline: `Project Website for my Ph.D. research`,
+  url: 'https://MeilianLee.github.io',
+  baseUrl: '/phd-project-web/',
   images: {
     domains: ["meilianlee.github.io"],
   },
